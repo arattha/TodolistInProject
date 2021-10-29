512c3e395cb69b8f161a12e0034f2b4cb856c0e3
h982 <ericharry@naver.com>
2021-10-29 15:47:05 +0900

    [S05P31F001-46][BE] fix: Request 수정
    2021.10.29
    Request에 필요한 데이터 추가

--- backend/src/main/java/com/web/tip/member/request/UpdateMemberRequest.java
+++ backend/src/main/java/com/web/tip/member/request/UpdateMemberRequest.java
@@ -1,4 +1,16 @@
 package com.web.tip.member.request;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@Builder
+@ToString
 public class UpdateMemberRequest {
+    private String id;
+    private String email;
+    private String phone;
 }
