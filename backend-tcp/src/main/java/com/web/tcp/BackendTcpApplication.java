68e4c015522f93eb54315fa243368da9313cec22
h982 <ericharry@naver.com>
2021-10-29 11:27:14 +0900

    [S05P31F001-23] new : tcp 서버 추가

---
+++ backend-tcp/src/main/java/com/web/tcp/BackendTcpApplication.java
@@ -0,0 +1,13 @@
+package com.web.tcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendTcpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendTcpApplication.class, args);
+    }
+
+}
