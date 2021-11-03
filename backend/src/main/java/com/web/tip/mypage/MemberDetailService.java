5a444a55da1ff34faeab3067eea67a14e84bc4b5
h982 <ericharry@naver.com>
2021-11-03 16:29:28 +0900

    [S05P31F001-100][BE] feat: 회원정보 수정
    2021.11.03
    - 프로필 이미지 변경기능 추가
    - 입력값이 없으면 변경안되게 수정

--- backend/src/main/java/com/web/tip/mypage/MemberDetailService.java
+++ backend/src/main/java/com/web/tip/mypage/MemberDetailService.java
@@ -2,21 +2,28 @@ package com.web.tip.mypage;
 
 import com.web.tip.error.CustomException;
 import com.web.tip.error.ErrorCode;
+import com.web.tip.image.ImgFileDto;
+import com.web.tip.image.ImgService;
 import com.web.tip.member.Member;
 import com.web.tip.member.MemberDao;
 import com.web.tip.member.request.SignUpRequest;
 import com.web.tip.member.request.UpdateMemberRequest;
-import com.web.tip.member.response.MyPageResponse;
 import com.web.tip.util.IdGenerator;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.util.Optional;
+
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class MemberDetailService {
     private final MemberDao memberDao;
     private final MemberDetailDao memberDetailDao;
     private final IdGenerator idGenerator;
+    private final ImgService imgService;
 
     public void createMemberDetail(Member member, SignUpRequest signUpRequest) {
         String id = idGenerator.generateId();
@@ -34,22 +41,38 @@ public class MemberDetailService {
         memberDetailDao.save(memberDetail);
     }
 
-    public MyPageResponse updateMemberDetail(UpdateMemberRequest updateMemberRequest) {
+    public MemberDetailDto updateMemberDetail(UpdateMemberRequest updateMemberRequest) {
         Member member = memberDao.findMemberById(updateMemberRequest.getId())
                 .orElseThrow(() -> new CustomException(ErrorCode.MEMBER_NOT_FOUND));
-        MemberDetail memberDetail = memberDetailDao.findMemberDetailByMember(member)
+
+        Optional.ofNullable(updateMemberRequest.getMultipartFile())
+                .ifPresent(multipartFile -> {
+                    try {
+                        imgService.addFile(ImgFileDto.builder()
+                                .id(member.getId())
+                                .multipartFile(multipartFile)
+                                .build()
+                        );
+                    } catch (IOException e) {
+                        log.error("saving profile Image failed");
+                        throw new CustomException(ErrorCode.INTERNAL_SERVER_ERROR);
+                    }
+                });
+
+        MemberDetail memberDetail = memberDetailDao.findMemberDetailByMemberId(updateMemberRequest.getId())
                 .orElseThrow(() -> new CustomException(ErrorCode.MEMBER_NOT_FOUND));
 
         MemberDetail updatedMember = MemberDetail.builder()
                 .member(member)
                 .id(memberDetail.getId())
-                .phone(updateMemberRequest.getPhone()==null? memberDetail.getPhone() : updateMemberRequest.getPhone())
-                .email(updateMemberRequest.getEmail()==null? memberDetail.getEmail() : updateMemberRequest.getEmail())
+                .phone(updateMemberRequest.getPhone() == null ? memberDetail.getPhone() : updateMemberRequest.getPhone())
+                .email(updateMemberRequest.getEmail() == null ? memberDetail.getEmail() : updateMemberRequest.getEmail())
+                .profileImg(memberDetail.getProfileImg())
                 .build();
 
         memberDetailDao.save(updatedMember);
 
-        return MyPageResponse.entityToResponse(member, MemberDetailDto.entityToDto(updatedMember));
+        return MemberDetailDto.entityToDto(updatedMember);
     }
 
     public MemberDetailDto findMemberDetail(Member member) {
