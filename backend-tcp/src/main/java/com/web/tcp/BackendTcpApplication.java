4f24e1ee96a9953f4de654aef153a5de702be294
조성표 <pyo7410@gmail.com>
2021-11-05 11:49:31 +0900

    [BE] fix : 배포를 위한 내용 수정
    - BackendTcpApplication에 pid를 생성하는 기능 추가
    
    2021.11.05

--- backend-tcp/src/main/java/com/web/tcp/BackendTcpApplication.java
+++ backend-tcp/src/main/java/com/web/tcp/BackendTcpApplication.java
@@ -2,12 +2,18 @@ package com.web.tcp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class BackendTcpApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BackendTcpApplication.class, args);
+
+        //SpringApplication.run(BackendApplication.class, args);
+        SpringApplication app = new SpringApplication(BackendTcpApplication.class);
+        app.addListeners(new ApplicationPidFileWriter());
+        app.run(args);
+
     }
 
 }
