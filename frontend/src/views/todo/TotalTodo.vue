1e35eeec1d001244301b5abc4bbaba7c5390a70c
조성표 <pyo7410@gmail.com>
2021-11-05 11:44:04 +0900

    [FE] test: Test 커밋
    2021.11.05

--- frontend/src/views/todo/TotalTodo.vue
+++ frontend/src/views/todo/TotalTodo.vue
@@ -135,7 +135,7 @@ export default {
   methods: {
     ...mapActions(['set_project_name', 'set_project_id']),
     connect() {
-      const serverURL = 'https://localhost:8082/todo';
+      const serverURL = 'http://localhost:8082/todo';
       let socket = new SockJS(serverURL);
       this.stompClient = Stomp.over(socket, { debug: false });
       this.stompClient.connect(
