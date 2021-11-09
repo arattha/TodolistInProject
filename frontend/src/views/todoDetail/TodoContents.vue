4a30d3c6b1a63087fb22e69188b8bbaf06854c88
조성표 <pyo7410@gmail.com>
2021-11-09 18:55:56 +0900

    [S05P31F001-155][FE] feat: 할일 상세 내용 수정
    2021.11.09
    - 할일 상세 내용 추가 시 즉각 반영되도록 수정
    - 모달 닫을 시 NavigationDuplicated가 발생하는 문제 수정
    - 추가 시 모달이 바로 닫히도록 수정

--- frontend/src/views/todoDetail/TodoContents.vue
+++ frontend/src/views/todoDetail/TodoContents.vue
@@ -6,6 +6,7 @@
 
 <script>
 import TodoDetailContents from '@/components/TodoDetailContents.vue';
+import { mapGetters, mapActions } from 'vuex';
 import { getTodoContent } from '@/api/todo.js';
 import { formatDate } from '@/api/utils.js';
 
@@ -21,30 +22,16 @@ export default {
     todoId: {
       handler(id) {
         if (id) {
-          getTodoContent(
-            id,
-            (res) => {
-              this.detailList = [];
-              for (let i = 0; i < res.object.length; i++) {
-                let item = res.object[i];
-                let temp = {
-                  id: item['id'],
-                  name: item['writer'],
-                  content: item['contents'],
-                  regDate: formatDate(item['regDate']),
-                  profileImg: item['profileImg'],
-                };
-                this.detailList.push(temp);
-              }
-            },
-            (error) => {
-              alert('Todo 상세 목록 받아오는데 문제가 발생했습니다. 새로고침 해주세요!!');
-              console.log(error);
-            }
-          );
+          this.getTodoDetail(id);
         }
       },
       immediate: true,
+    },
+    reloadTodoDetail: async function (val) {
+      if (val) {
+        await this.getTodoDetail(this.todoId);
+        await this.toggle_reload_todo_detail(false);
+      }
     },
   },
   data() {
@@ -52,6 +39,35 @@ export default {
       detailList: [],
       todoId: '',
     };
+  },
+  computed: {
+    ...mapGetters(['reloadTodoDetail']),
+  },
+  methods: {
+    ...mapActions(['toggle_reload_todo_detail']),
+    getTodoDetail(id) {
+      getTodoContent(
+        id,
+        (res) => {
+          this.detailList = [];
+          for (let i = 0; i < res.object.length; i++) {
+            let item = res.object[i];
+            let temp = {
+              id: item['id'],
+              name: item['writer'],
+              content: item['contents'],
+              regDate: formatDate(item['regDate']),
+              profileImg: item['profileImg'],
+            };
+            this.detailList.push(temp);
+          }
+        },
+        (error) => {
+          alert('Todo 상세 목록 받아오는데 문제가 발생했습니다. 새로고침 해주세요!!');
+          console.log(error);
+        }
+      );
+    },
   },
 };
 </script>
