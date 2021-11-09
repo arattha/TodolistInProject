d2fecd2080b70b6d95144e7bfe9678407f5b0fb1
조용일 <cyi1207@naver.com>
2021-11-09 17:38:20 +0900

    hotfix : WebSocketConfig 수정
    2021.11.09 주소 수정[배포 서버 테스트]
       - /tcpapi 삭제

--- backend-tcp/src/main/java/com/web/tcp/config/WebSocketConfig.java
+++ backend-tcp/src/main/java/com/web/tcp/config/WebSocketConfig.java
@@ -22,7 +22,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     // connection을 맺을때 CORS 허용
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/socketalarm").setAllowedOrigins("https://k5f006.p.ssafy.io/tcpapi").withSockJS();
+        registry.addEndpoint("/socketalarm").setAllowedOrigins("https://k5f006.p.ssafy.io").withSockJS();
         registry.addEndpoint("/todo").setAllowedOrigins("https://k5f006.p.ssafy.io").withSockJS();
     }
 
